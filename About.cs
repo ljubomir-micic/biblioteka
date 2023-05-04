ba2460a63ef96ad142fcb2f4a6307cc804504dd5
Љубомир <94759247+MiLjubomir@users.noreply.github.com>
2023-05-04 11:17:38 +0200

    Update About.cs

--- About.cs
+++ About.cs
@@ -5,10 +5,14 @@
 //
 ////////////////////////////////////////
 
+using Project.Time;
+
 namespace Project {
   public static class About {
     public static void main(string[] args) {
       Console.WriteLine("Програм направљен за матурски испит.\nОвај део је добар с тим што IDE који користим прави нелогичне багове које бих додатно требао да исправљам. Он је и схифар.\nПрелазим на Visual Studio.");
+      Time.Forward(96); // in days
+      Console.WriteLine("Завршен пројекат");
     }
   }
 }
